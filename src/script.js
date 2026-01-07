3015b063fef2fb6b62f93ce66df1450b8e1c4ba2
Bahauddin Mandal <mandalbahauddin02@gmail.com>
2026-01-07 14:17:51 +0530

    1st commit

---
+++ src/script.js
@@ -0,0 +1,39 @@
+const signUpCard = document.getElementById("signUpCard");
+const successMsg = document.getElementById("successMsg");
+const emailForm = document.getElementById("emailForm");
+const emailInput = document.getElementById("emailInput");
+const errMsg = document.getElementById("errMsg");
+const successEmail = document.getElementById("successEmail");
+const dismissBtn = document.getElementById("dismissBtn");
+
+emailForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = emailInput.value.trim();
+  const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+
+  if (!isValidEmail) {
+    emailInput.classList.add("err-input", "animate-shake");
+    errMsg.innerText = "valid email required";
+    emailInput.addEventListener("animationend", () => {
+      emailInput.classList.remove("animate-shake");
+    });
+    return;
+  } else {
+    emailInput.classList.remove("err-input");
+    errMsg.innerText = "";
+  }
+  successEmail.textContent = email;
+
+  signUpCard.classList.add("animate-fade-out");
+  setTimeout(() => {
+    signUpCard.classList.remove("animate-fade-out");
+    signUpCard.style.display = "none";
+    successMsg.style.display = "flex";
+  }, 300);
+});
+
+dismissBtn.addEventListener("click", function () {
+  signUpCard.style.display = "flex";
+  successMsg.style.display = "none";
+  emailInput.value = "";
+});
